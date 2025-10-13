0fe3a6aba8f89a968202c4765116d473b852c0a7
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-13 22:26:10 +0530

    updated gateway

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       </h2>
       <div className="flex flex-col md:flex-row w-full justify-center items-center gap-6">
         <div className="w-full max-w-md">
-        {/* <CredentialsTable credentials={credentials} /> */}
+        <CredentialsTable credentials={credentials} />
         </div>
         <div className="w-full  max-w-md flex flex-col gap-6">
           <Link href="/issue-cred" className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 text-center">
