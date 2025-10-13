dd82bd62720609d9f5efd95ee1fbb20de357ffbe
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-13 22:06:36 +0530

    updated gateway

--- backend/gateway/app.ts
+++ backend/gateway/app.ts
@@ -1,8 +1,34 @@
 import express, { Application, Request, Response } from "express";
 import expressProxy from "express-http-proxy";
+import cors from 'cors';
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
+
+dotenv.config();
 const app: Application = express();
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the build folder
+    app.use(express.static(path.join(__dirname, 'dist')));
+    
+    // Handle React routing - send all non-API requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    });
+} else {
+    
+}
 app.use("/api/issue", expressProxy(`${process.env.ISSUE_SERVICE_URL}`));
 app.use("/api/verify", expressProxy(`${process.env.VERIFY_SERVICE_URL}`));
 app.use("/", (req: Request, res: Response) => {
@@ -12,3 +38,4 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Gateway server listening on port ${PORT}`);
 });
+
