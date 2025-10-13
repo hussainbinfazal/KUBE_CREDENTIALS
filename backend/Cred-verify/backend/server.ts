7a407fb6f4a6acf6e194066530a85fce03d8a84e
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-13 22:50:03 +0530

    updated gateway

--- backend/Cred-verify/backend/server.ts
+++ backend/Cred-verify/backend/server.ts
@@ -6,8 +6,8 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
 import cookieParser from 'cookie-parser';
 import rateLimit from 'express-rate-limit';
@@ -47,17 +47,17 @@ app.use('/', credVerifyRoutes);
 app.get('/health', (req, res) => {
   res.json({ service: 'cred-verify', status: 'ok' });
 });
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'dist')));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'dist')));
     
     
-    app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  });
-} else {
+//     app.get(/.*/, (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+//   });
+// } else {
     
-}
+// }
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
